77718ee83a5dcf9e3307381a4e686440b894df8b
Galina Popova <galina.popova@offerrum.com>
2025-06-04 17:06:38 +0300

    add

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import '../src/style.css'
 import javascriptLogo from '../src/javascript.svg'
 import viteLogo from './vite.svg'
-import { setupCounter } from '../src/counter.js'
+import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
